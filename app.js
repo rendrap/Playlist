2301fb4dc9781a793c8bb2de62ea0dd8717c5d8e
rendrap <rendrapradana@gmail.com>
2018-12-09 11:21:25 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var playlist = new Playlist();
+
+var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2:54");
+var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the waves", "3:43");
+
+playlist.add(hereComesTheSun);
+playlist.add(walkingOnSunshine);
+
+var playlistElement = document.getElementById("playlist");
+
+playlist.renderIn(playlistElement);
